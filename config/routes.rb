6d42b0532475a399db0377469bdafe1eb3f93271
martaf793 <martaf@uchicago.edu>
2025-04-27 14:27:53 +0000

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   get("/wizard_multiply", { :controller => "multiplication", :action => "multiply_these" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
-  get("/wizard_div", { :controller => "div", :action => "divide_these" })
+  get("/wizard_div", { :controller => "division", :action => "divide_these" })
 
 end
